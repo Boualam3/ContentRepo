249349ddbb38be1eec3dbadc58cc993be3a3634d
Boualam BR <boualam.bourahla@gmail.com>
2022-07-14 21:32:00 +0100

    upload js file -

---
+++ script.js
@@ -0,0 +1,6 @@
+const header = document.getElementById('header')
+// const headerBg = document.getElementById('header-bg')
+
+if (header.classList.contains('animated-bg')) {
+  header.style.display = 'none'
+}

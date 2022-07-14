0303b9fcbc9e370008bf22c6431ac03fedf6e249
Boualam BR <boualam.bourahla@gmail.com>
2022-07-14 21:39:04 +0100

    latest changed

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const header = document.getElementById('header')
 // const headerBg = document.getElementById('header-bg')
 
 if (header.classList.contains('animated-bg')) {
-  header.style.display = 'none'
+  header.childNodes[1].style.display = 'none'
 }
